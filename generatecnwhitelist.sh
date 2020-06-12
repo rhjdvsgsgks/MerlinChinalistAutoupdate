a26d8f4d19d77f55fd3180d6d66c7094dfce5c94
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2020-06-12 09:01:47 -0500

    Update generatecnwhitelist.sh

--- generatecnwhitelist.sh
+++ generatecnwhitelist.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ispdns1=$(nvram get wan0_dns|awk '{print $1}')
-cat /koolshare/ss/rules/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns1#53/g" > /jffs/configs/dnsmasq.d/cnwhitelist.conf
+ispdns2=$(nvram get wan0_dns|awk '{print $2}')
+cat /koolshare/ss/rules/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns1/g" > /jffs/configs/dnsmasq.d/cnwhitelist.conf
+cat /koolshare/ss/rules/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns2/g" >> /jffs/configs/dnsmasq.d/cnwhitelist.conf
