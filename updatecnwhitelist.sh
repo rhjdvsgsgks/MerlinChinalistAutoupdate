543f0664714528eff201b9da43dbb9ad636c3eed
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2021-08-30 19:10:06 +0000

    sync changes of path

--- updatecnwhitelist.sh
+++ updatecnwhitelist.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 remotemd5=$(wget https://raw.githubusercontent.com/hq450/fancyss/master/rules/version1 -qO - | grep cdn |awk '{print $3}')
 update(){
-if [ $remotemd5 != $(md5sum /koolshare/ss/rules/cdn.txt | awk '{print $1}') ]
+if [ $remotemd5 != $(md5sum /jffs/cdn.txt | awk '{print $1}') ]
 then
 	logger -t updatecnwhitelist -p notice 'updating cnwhitelist'
-	wget https://raw.githubusercontent.com/hq450/fancyss/master/rules/cdn.txt -qO /koolshare/ss/rules/cdn.txt
+	wget https://raw.githubusercontent.com/hq450/fancyss/master/rules/cdn.txt -qO /jffs/cdn.txt
 	logger -t updatecnwhitelist -p notice 'download finished'
 	update
 else
