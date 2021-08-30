3e764b54f7ccec8aed219316e62c3ccebcb7b3fe
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2021-08-30 19:06:27 +0000

    support ipv6_get_dns

--- generatecnwhitelist.sh
+++ generatecnwhitelist.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ispdns1=$(nvram get wan0_dns|awk '{print $1}')
 ispdns2=$(nvram get wan0_dns|awk '{print $2}')
-cat /koolshare/ss/rules/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns1/g" > /jffs/configs/dnsmasq.d/cnwhitelist.conf
-cat /koolshare/ss/rules/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns2/g" >> /jffs/configs/dnsmasq.d/cnwhitelist.conf
+ispdns3=$(nvram get ipv6_get_dns|awk '{print $1}')
+ispdns4=$(nvram get ipv6_get_dns|awk '{print $2}')
+cat /jffs/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns1/g" > /jffs/configs/dnsmasq.d/cnwhitelist.conf
+cat /jffs/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns2/g" >> /jffs/configs/dnsmasq.d/cnwhitelist.conf
+cat /jffs/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns3/g" >> /jffs/configs/dnsmasq.d/cnwhitelist.conf
+cat /jffs/cdn.txt | sed 's/^/server=\/./g' | sed "s/$/\/$ispdns4/g" >> /jffs/configs/dnsmasq.d/cnwhitelist.conf
 service restart_dnsmasq
